51210cb7e3e32602b72e0fce02939c72dc8e29d9
LaVon Mejia <lavonmejia@gmail.com>
2019-06-27 19:04:32 -0400

    still trying to get modals to work

--- js/index.js
+++ js/index.js
@@ -141,6 +141,48 @@ if (carousel) {
 }
 
 
-document.querySelector(".donate-button").addEventListener("click", function() {
+document.querySelectorAll(".donate-button").addEventListener("click", function() {
   alert("Please Sign In to Complete Your Giving");
 });
+
+
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+  }
+
+
+
+
+
+
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
